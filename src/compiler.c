717b9f95e79a87cca2372b523146473662f6887a
Ilya <dorogino@gmail.com>
2024-12-16 01:26:46 +0700

    dev types of value

--- src/compiler.c
+++ src/compiler.c
@@ -160,6 +160,15 @@ static void binary() {
     }   
 }
 
+static void literal() {
+    switch (parser.previous.type) {
+        case TOKEN_FALSE: emitByte(OP_FALSE); break;
+        case TOKEN_NIL: emitByte(OP_NIL); break;
+        case TOKEN_TRUE: emitByte(OP_TRUE); break;
+        default: return;
+    }
+}
+
 static void grouping() {
     //* Для обработки выражения в скобках
     expression();
@@ -169,7 +178,7 @@ static void grouping() {
 static void number() {
     //* Мы предполагаем, что токен для числового литерала уже был использован и хранится в previous, а затем преобразуем его в double
     double value = strtod(parser.previous.start, NULL);
-    emitConstant(value);
+    emitConstant(NUMBER_VAL(value));
 }
 
 static void unary() {
@@ -181,6 +190,7 @@ static void unary() {
 
     //* Выдать инструкцию оператора
     switch (operatorType) {
+        case TOKEN_BANG: emitByte(OP_NOT); break;
         case TOKEN_MINUS: emitByte(OP_NEGATE); break;
         default: return; //* 
     }
@@ -199,7 +209,7 @@ ParseRule rules[] = {
     [TOKEN_SEMICOLON]     = {NULL,     NULL,   PREC_NONE},
     [TOKEN_SLASH]         = {NULL,     binary, PREC_FACTOR},
     [TOKEN_STAR]          = {NULL,     binary, PREC_FACTOR},
-    [TOKEN_BANG]          = {NULL,     NULL,   PREC_NONE},
+    [TOKEN_BANG]          = {unary,     NULL,   PREC_NONE},
     [TOKEN_BANG_EQUAL]    = {NULL,     NULL,   PREC_NONE},
     [TOKEN_EQUAL]         = {NULL,     NULL,   PREC_NONE},
     [TOKEN_EQUAL_EQUAL]   = {NULL,     NULL,   PREC_NONE},
@@ -213,17 +223,17 @@ ParseRule rules[] = {
     [TOKEN_AND]           = {NULL,     NULL,   PREC_NONE},
     [TOKEN_CLASS]         = {NULL,     NULL,   PREC_NONE},
     [TOKEN_ELSE]          = {NULL,     NULL,   PREC_NONE},
-    [TOKEN_FALSE]         = {NULL,     NULL,   PREC_NONE},
+    [TOKEN_FALSE]         = {literal,     NULL,   PREC_NONE},
     [TOKEN_FOR]           = {NULL,     NULL,   PREC_NONE},
     [TOKEN_FUN]           = {NULL,     NULL,   PREC_NONE},
     [TOKEN_IF]            = {NULL,     NULL,   PREC_NONE},
-    [TOKEN_NIL]           = {NULL,     NULL,   PREC_NONE},
+    [TOKEN_NIL]           = {literal,     NULL,   PREC_NONE},
     [TOKEN_OR]            = {NULL,     NULL,   PREC_NONE},
     [TOKEN_PRINT]         = {NULL,     NULL,   PREC_NONE},
     [TOKEN_RETURN]        = {NULL,     NULL,   PREC_NONE},
     [TOKEN_SUPER]         = {NULL,     NULL,   PREC_NONE},
     [TOKEN_THIS]          = {NULL,     NULL,   PREC_NONE},
-    [TOKEN_TRUE]          = {NULL,     NULL,   PREC_NONE},
+    [TOKEN_TRUE]          = {literal,     NULL,   PREC_NONE},
     [TOKEN_VAR]           = {NULL,     NULL,   PREC_NONE},
     [TOKEN_WHILE]         = {NULL,     NULL,   PREC_NONE},
     [TOKEN_ERROR]         = {NULL,     NULL,   PREC_NONE},
--- src/vm.c
+++ src/vm.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdarg.h>
 
 #include "common.h"
 #include "compiler.h"
@@ -10,6 +11,20 @@ VM vm;
 static void resetStack() {
     // Обнуление стека
     vm.stackTop = vm.stack;
+}
+
+static void runtimeError(const char* format, ...) {
+    //* Сообщение об ошибке времени выполнения
+    va_list args;   //* Произвольное число аргументов
+    va_start(args, format); //* указание на последний обязательный аргумент
+    vfprintf(stderr, format, args);
+    va_end(args);   //* Выход из функции с переменным списком аргументов
+    fputs("\n", stderr);
+
+    size_t instruction = vm.ip - vm.chunk->code - 1;    //* Текущая инструкция, которая вызвала ошибку
+    int line = vm.chunk->lines[instruction];
+    fprintf(stderr, "[line %d] in script\n", line);
+    resetStack();
 }
 
 void initVM() {
@@ -33,17 +48,31 @@ Value pop() {
     return *vm.stackTop;
 }
 
+static Value peek(int distance) {
+    //* Возвращает значение из стека, но не извлекает его
+    return vm.stackTop[-1 - distance];
+}
+
+static bool isFalsey(Value value) {
+    //* nil и false ложны, а любое другое значение ведет себя как true
+    return IS_NIL(value) || (IS_BOOL(value) && !IS_BOOL(value));
+}
+
 static InterpretResult run() {
     // Выполнение чанка кода
     #define READ_BYTE() (*vm.ip++)  // Сначала разымёвываем инструкцию, потом увеличиваем указатель
     #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])   // считывает следующий байт из байт-кода
     // Простая бинарная операция с двумя операндами
     // Использование do-while для возможности содержать несколько операторов в одном блоке 
-    #define BINARY_OP(op) \
+    #define BINARY_OP(valueType, op) \
         do { \
-            double b = pop(); \
-            double a = pop(); \
-            push(a op b); \
+            if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
+                runtimeError("OPerands must be numbers."); \
+                return INTERPRET_RUNTIME_ERROR; \
+            } \
+            double b = AS_NUMBER(pop()); \
+            double a = AS_NUMBER(pop()); \
+            push(valueType(a op b)); \
         } while (false); 
     for(;;) {
         #ifdef DEBUG_TRACE_EXECUTION
@@ -65,11 +94,23 @@ static InterpretResult run() {
                 push(constant);
                 break;
             }
-            case OP_ADD:        BINARY_OP(+); break;
-            case OP_SUBTRACT:   BINARY_OP(-); break;
-            case OP_MULTIPLY:   BINARY_OP(*); break;
-            case OP_DIVIDE:     BINARY_OP(/); break;
-            case OP_NEGATE:     push(-pop()); break;
+            case OP_NIL:        push(NIL_VAL); break;
+            case OP_TRUE:       push(BOOL_VAL(true)); break;
+            case OP_FALSE:      push(BOOL_VAL(false)); break;
+            case OP_ADD:        BINARY_OP(NUMBER_VAL, +); break;
+            case OP_SUBTRACT:   BINARY_OP(NUMBER_VAL, -); break;
+            case OP_MULTIPLY:   BINARY_OP(NUMBER_VAL, *); break;
+            case OP_DIVIDE:     BINARY_OP(NUMBER_VAL, /); break;
+            case OP_NOT:
+                push(BOOL_VAL(isFalsey(pop())));
+                break;
+            case OP_NEGATE:
+                if (!IS_NUMBER(peek(0))) {
+                    runtimeError("Operand must be a number.");
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                push(NUMBER_VAL(-AS_NUMBER(pop())));
+                break;
             case OP_RETURN: {
                 printValue(pop());
                 printf("\n");
