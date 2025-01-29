639c35417aae122ea69c33078edf5def3022a1e2
Johan <dorogino@gmail.com>
2025-01-29 22:54:35 +0700

    dev hash

--- src/table.c
+++ src/table.c
@@ -6,9 +6,35 @@
 #include "table.h"
 #include "value.h"
 
+#define TABLE_MAX_LOAD 0.75
+
 void initTable(Table* table) {
     //* Инициализация хэш-таблицы
     table->count = 0;
     table->capacity = 0;
     table->entries = NULL;
+}
+
+void freeTable(Table* table) {
+    FREE_ARRAY(Entry, table->entries, table->capacity);
+    initTable(table);
+}
+
+static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
+    uint32_t index = key->hash % capacity;
+}
+
+bool tableSet(Table* table, ObjString* key, Value value) {
+    //* Вставка записи в хэш-таблицу
+    if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
+        int capacity = GROW_CAPACITY(table->capacity);
+        adjustCapacity(table, capacity);
+    }
+    Entry* entry = findEntry(table->entries, table->capacity, key);
+    bool isNewKey = entry->key == NULL;
+    if (isNewKey) table->count++;
+
+    entry->key = key;
+    entry->value = value;
+    return isNewKey;
 }
